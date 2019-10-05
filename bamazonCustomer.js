8f3d1a3a15c06eb3b452265577897a786d60b332
rachel doran <rmitchell0925@gmail.com>
2019-10-05 14:52:15 -0400

    modified js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -66,31 +66,31 @@ function postProduct() {
         type: "input",
         message: "How many you want?"
       },
-      {
-        name: "startingBid",
-        type: "input",
-        message: "What would you like your starting bid to be?",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      }
+      // {
+      //   name: "startingBid",
+      //   type: "input",
+      //   message: "What would you like your starting bid to be?",
+      //   validate: function(value) {
+      //     if (isNaN(value) === false) {
+      //       return true;
+      //     }
+      //     return false;
+      //   }
+      // }
     ])
     .then(function(answer) {
       // when finished prompting, insert a new item into the db with that info
       connection.query(
-        "INSERT INTO auctions SET ?",
+        "INSERT INTO products SET ?",
         {
-          item_name: answer.item,
-          category: answer.category,
-          starting_bid: answer.startingBid || 0,
-          highest_bid: answer.startingBid || 0
+          product_name: answer.item,
+          department_name: answer.department,
+          price: answer.price|| 0,
+          stock_quantity: answer.quantity || 0
         },
         function(err) {
           if (err) throw err;
-          console.log("Your auction was created successfully!");
+          console.log("Your product was added successfully!");
           // re-prompt the user for if they want to bid or post
           start();
         }
